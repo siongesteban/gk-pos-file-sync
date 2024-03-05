28c55eb8c6f6ad1dcdfa92c47f57926831858196
siongesteban <siong.esteban@gmail.com>
2024-03-05 18:46:17 +0800

    chore: include env and restaurant id in sqs names

--- src/app/sqs-names.ts
+++ src/app/sqs-names.ts
@@ -1,4 +1,6 @@
+import env from './env.json';
+
 export const SqsNames = {
-  DOWNLOAD_MENU_FILE: 'download-menu-file',
-  DOWNLOAD_ORDER_FILE: 'download-order-file',
+  DOWNLOAD_MENU_FILE: `${env.env}-${env.restaurantId}-download-menu-file`,
+  DOWNLOAD_ORDER_FILE: `${env.env}-${env.restaurantId}-download-order-file`,
 };
